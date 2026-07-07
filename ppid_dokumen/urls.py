15b91db59c357e29775d547797cab572e90bdd74
Arief Rachman <arief.rachman@ar-raniry.ac.id>
2026-07-07 10:36:26 +0700

    commit update url utama ke tabel CDN

--- ppid_dokumen/urls.py
+++ ppid_dokumen/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "ppid_dokumen"
 
 urlpatterns = [
-    path("", views.document_list, name="list"),
+    # path("", views.document_list, name="list"),
     path('data/', views.list_sftp_files_html, name='list_cdn_files'),
-    path('data/tabel/', views.cdn_files_table, name='cdn_files_table'),
+    path('', views.cdn_files_table, name='cdn_files_table'),
     path("unduh/<int:pk>/", views.document_download, name="download"),
 ]
